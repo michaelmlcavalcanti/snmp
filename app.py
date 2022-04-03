c27292805f188274e4df590ed4e0549d697ddeef
Matheus Phelipe <matheusphalves@gmail.com>
2022-04-03 13:53:11 -0300

    coding snmp message on get_request method

--- app.py
+++ app.py
@@ -7,31 +7,17 @@ app = Flask(__name__)
 snmp = SnmpManager(timeout=10)
 logging.basicConfig(level=logging.INFO)
 
-@app.route('/get_oid_list', methods=['POST'])
-def get_oid_list():
+@app.route('/get_request', methods=['POST'])
+def get_request():
     request_body = request.json
     ip_address = request_body['ip_address']
     community = request_body['community']
-    logging.info(f'Finding oid list from IP {ip_address} with community: {community}')
+    oid = request_body['oid']
+    logging.info(f'Sending GET REQUEST to ({ip_address}, {community}, {oid})')
+    status, oid_response = snmp.get_request(ip_address=ip_address, community=community, oid=oid)
     
-    status, oid_list = snmp.get_oid_list(ip_address=ip_address, community=community)
-
-    return {'status': status, 'oid_list': oid_list}, [200 if status else 400]   #json response, status_code
-
-
-@app.route('/get_oid_info', methods=['POST'])
-def get_oid():
-    request_body = request.json
-    ip_address = request_body['ip_address']
-    oid_list = request_body['oid_list']
-    #adicionar schema pra validar json
-    print(ip_address)
-    print(oid_list)
-    return 'ola', 200   #json response, status_code
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    print(status, oid_response)
+    return {'status': status, 'response': oid_response} 
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080)
--- service/snmp_manager.py
+++ service/snmp_manager.py
@@ -4,7 +4,7 @@ import logging
 
 
 
-class SnmpManager():
+class SnmpManager(BaseException):
     '''Class define ....'''
 
     def __init__(self, timeout,  port = 161) -> None:
@@ -24,28 +24,73 @@ class SnmpManager():
             logging.error(f'Error during SnmpManager initialization: {ex}') 
             return False
 
-    def get_oid_list(self, ip_address, community):
-        '''Return a list of oid'''
-        oid_list = []
+    def get_request(self, ip_address, community, oid):
         status = False
+        snmp_response = None
         
         if(self.is_ready):
-            if (community is ''):
+            if (community == ''):
+                logging.info('Using default community: public')
                 community = 'public'
+
+            OID = oid.replace('.', '')
+            lenOID = len(OID)
+            lenComm = len(community)
+            #montar msg
+            SVal = '50'
+            #Objeto Field
+            TypeOId = chr(0x06)
+            SOid = TypeOId + chr(lenOID) + OID
+
+            #Sequence / Varbind Type Field
+            TypeVarbind = chr(0x30) # tipo varbind
+            SVarbind = TypeVarbind + chr(lenOID + 2 + 2) + SOid + SVal
+
+            #Sequence / Varbind List Field
+            TypeVarbindList = chr(0x30) # tipo varbind list
+            SVarbindList = TypeVarbindList + chr(len(SVarbind)) + SVarbind
+
+            #campos Request ID, Error, ErrorIndex
+            RqID = chr(2) + chr(1) + chr(1)
+            Err = chr(2) + chr(1) + chr(0)
+            ErrIndex = chr(2) + chr(1) + chr(0)
+
+            SPDU = chr(0xa0) + chr(3 + 3 + 3 + len(SVarbindList)) + RqID + Err + ErrIndex + SVarbindList
+
+            #Community
+            SComm = chr(4) + chr(lenComm) + community
+
+            #Versao
+            SVersao = chr(2) + chr(1) + chr(0)
+
+            #SNMP MESSAGE
+            MsgType = chr(0x30)
+            SSnmp = MsgType + chr(3 + 2 + lenComm + len(SPDU)) + SVersao + SComm + SPDU
+
+            str2byte = SSnmp.encode()
+            print(str2byte)
+            self.snmp_socket.sendto(str2byte, (ip_address, 161))
 
             while True:
                 try:
                     snmp_response = self.snmp_socket.recv(2000).decode()
                     logging.info('SNMP Message received!')
-                    oid_list = self.__create_oid_list(snmp_response) 
-                except self.snmp_socket:
-                    logging.error(f'The socket timed out ({self.timeout})')
+                    snmp_response = self.__handle_snmp_response(snmp_response) 
+                    break
+                except Exception as ex:
+                    if(self.snmp_socket.timeout):
+                        logging.info(f'The socket timed out ({self.timeout})')
+                    else: 
+                        logging.info(f'Unhandled exception: {ex}')
+                    break
 
+        return status, snmp_response
 
-        return status, oid_list
-
-    def get_oid_info(self, ip_address, community, oid):
+    def __handle_snmp_response(self, snmp_response):
         pass
 
     def __create_oid_list(self, oid_response):
         return [{'oid': oid[0], 'description': oid[1]} for oid in oid_response]
+
+    def __handle_oid_sequence(oid):
+        return oid.replace('.', '')
