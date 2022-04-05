eb8a0047838611ca15ff084548c52dba3409165c
Murilo Stodolni <m_campanhol@hotmail.com>
2022-04-04 23:14:12 -0300

    add button and form in App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
 
 function App() {
+  const [informations, setInformations] = useState()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SNMP</h1>
+        <Form>
+          <div key={`inline-radio`} className="mb-3">
+            <Form.Check
+              inline
+              label="Temperatura"
+              name="group1"
+              type="radio"
+              id={`inline-radio-1`}
+            />
+            <Form.Check
+              inline
+              label="HostName"
+              name="group1"
+              type="radio"
+              id={`inline-radio-2`}
+            />
+            <Form.Check
+              inline
+              label="Other Information"
+              name="group1"
+              type="radio"
+              id={`inline-radio-3`}
+            />
+          </div>
+        </Form>
+        <Button variant="primary" onClick={() => setInformations("informações vindas do agente!!!")}>Buscar Informação</Button>
+
+        {
+          informations
+        }
       </header>
     </div>
   );
