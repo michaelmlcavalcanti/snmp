3b16d2b76a59a73f8c185d763adcdc1730cbeec1
Murilo Stodolni <m_campanhol@hotmail.com>
2022-04-07 13:20:11 -0300

    test integration back with front

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -17,21 +17,39 @@ function App() {
     }])
   }
 
+  function post(e) {
+    e.preventDefault();
+    
+    fetch("http://127.0.0.1:8080/get_request", {
+      method: 'POST',
+      mode: 'no-cors',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'ip_address': '127.0.0.1',
+        'community': 'public',
+        'oid': '1.3.6.1.2.1.1.5.0'
+      })
+    })
+    .then((response) => {console.log(response)})
+    .catch((error) => console.log( error.response.request._response ) );
+  }
+
+
   return (
     <>
       <h1>SNMP</h1>
-        <ClayForm onSubmit={insertHost}>
-          <ClayForm.Group>
-            <ClayInput
-              id="ip"
-              placeholder="Ip"
-              type="text"
-            />
-          </ClayForm.Group>
-          <ClayButton className="mt-3 mb-4" type='submit' displayType="primary">
-            Add
-          </ClayButton>
-        </ClayForm>
+      <ClayForm onSubmit={post}>
+        <ClayForm.Group>
+          <ClayInput
+            id="ip"
+            placeholder="Ip"
+            type="text"
+          />
+        </ClayForm.Group>
+        <ClayButton className="mt-3 mb-4" type='submit' displayType="primary">
+          Add
+        </ClayButton>
+      </ClayForm>
       {
         hosts.map((host, index) => (
           <ClayCard key={index}>
